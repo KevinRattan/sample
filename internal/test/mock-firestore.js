0aeaf035e3b280c4f51903ac40ed5e9a278dfe9e
Kevin Rattan <kevin@time2yak.com>
2020-04-06 03:02:37 -0400

    initial tests

--- internal/test/mock-firestore.js
+++ internal/test/mock-firestore.js
@@ -1,11 +1,5 @@
-const Firestore = function () {
-    var entity = {};
-    entity.collection = collection;
-    return {
-        entity: entity
-    };
 
-}();
+const changeMe = { title: 'a firestore event', id: 1, description: 'something really cool', location: 'Joes pizza', likes: 0 };
 
 // mock events data - for a real solution this data should be coming 
 // from a cloud data store
@@ -13,13 +7,13 @@ const mockEvents = [
     {
         id: 1,
         data: function () {
-            return { title: 'an event', id: 1, description: 'something really cool', location: 'Joes pizza', likes: 0 };
+            return changeMe;
         }
     },
     {
         id: 2,
         data: function () {
-            return { title: 'another event', id: 2, description: 'something even cooler', location: 'Johns pizza', likes: 0 };
+            return { title: 'another firestore event', id: 2, description: 'something even cooler', location: 'Johns pizza', likes: 0 };
         }
     },
 
@@ -30,23 +24,40 @@ const snapshot = {
     empty: false
 }
 
-const collection = {
-    doc: function(id) {
-        return {
-            get: function() {
-                return mockEvents.find(x => x.id === id)
-            }
+const collection = function (arg) {
+    return {
+        doc: function (id) {
+            return  {
+                get: function () {
+                    return Promise.resolve(snapshot.docs.find(x => x.id === id));
+                },
+                update: function (arg) {
+                   changeMe.likes = arg.likes;
+                   return Promise.resolve(snapshot);
+                },
+            };
+        },
+        add: function (arg) {
+            snapshot.docs.push({
+                id: 3,
+                data: function () {
+                    return { title: arg.title, id: 2, description: arg.description, location: arg.location, likes: 0 };
+                } 
+            });
+            return Promise.resolve(snapshot);
+        },
+        get: function () {
+            return Promise.resolve(snapshot);
         }
-    },
-    add: function (arg) {
-        return Promise.resolve(snapshot);
-    },   
-    update: function (arg) {
-        return Promise.resolve(snapshot);
-    },   
-    get: function () {
-        return Promise.resolve(snapshot);
     }
 };
+
+const Firestore = function () {
+    var entity = {};
+    entity.collection = collection;
+    return entity;
+
+}();
+
 
 module.exports = Firestore;
--- internal/test/test-server.js
+++ internal/test/test-server.js
@@ -1,45 +1,55 @@
 var chai = require('chai');
 const request = require('supertest');
 const app = require('../server');
-const firestore = require('./mock-firestore');
+const sinon = require('sinon');
+const db = require('../repository');
 
-describe('GET /', function() {
-    it('responds with json', function(done) {
-      request(app)
-        .get('/')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    });
+describe('GET /', function () {
+  it('responds with json', function (done) {
+    request(app)
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
   });
+});
 
-  describe('GET /version', function() {
-    it('responds with the current version', function(done) {
-      request(app)
-        .get('/version')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-          chai.expect(JSON.parse(res.text)).to.eql({ version: '1.0.0' });
-          return done();
-        });
-    });
+describe('GET /version', function () {
+  it('responds with the current version', function (done) {
+    request(app)
+      .get('/version')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+        chai.expect(JSON.parse(res.text)).to.eql({ version: '1.0.0' });
+        return done();
+      });
   });
+});
 
-  describe('GET /events', function() {
-    it('responds with json', function(done) {
-      request(app)
-        .get('/events')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    });
-    it('returns events', function(done) {
-      request(app)
+describe('GET /events', function () {
+  let dbStub;
+  beforeEach(function() {
+    dbStub = sinon.stub(db, "getEvents");
+  });
+  afterEach(function() {
+    dbStub.restore();
+  });
+  it('responds with json', function (done) {
+    dbStub.returns(Promise.resolve({ events: []}));
+    request(app)
+      .get('/events')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+  it('returns events', function (done) {
+    dbStub.returns(Promise.resolve({ events: []}));
+    request(app)
       .get('/events')
       .set('Accept', 'application/json')
       .expect(200)
@@ -51,50 +61,44 @@ describe('GET /', function() {
         return done();
       });
 
-      });
   });
+});
 
-  describe('POST /event', function() {
-    it('adds an event', function(done) {
-      request(app)
+describe('POST /event', function () {
+  let dbStub;
+  beforeEach(function() {
+    dbStub = sinon.stub(db, "addEvent");
+  });
+  afterEach(function() {
+    dbStub.restore();
+  });
+  it('adds an event', function (done) {
+    dbStub.returns(Promise.resolve({ events: []}));
+    request(app)
       .post('/event')
-      .send( { title: 'a test event', description: 'a really cool test', location: 'Somewhere nice', likes: 0  })
+      .send({ title: 'a test event', description: 'a really cool test', location: 'Somewhere nice', likes: 0 })
       .set('Accept', 'application/json')
       .expect(200)
       .end((err, res) => {
         if (err) {
           return done(err);
         }
-        chai.expect(JSON.parse(res.text).events.length).to.equal(3);
         return done();
       });
-
-      });
   });
+});
 
-
-  describe('POST /event/like', function() {
-    it('likes an event', function(done) {
-      request(app)
-      .post('/event/like')
-      .send({ id: 2 })
-      .set('Accept', 'application/json')
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        chai.expect(JSON.parse(res.text).events.find(x => x.id === 2).likes).to.equal(1);
-        return done();
-      });
-
-      });
+describe('DELETE /event/like', function () {
+  let dbStub;
+  beforeEach(function() {
+    dbStub = sinon.stub(db, "removeLike");
   });
-
-
-  describe('DELETE /event/like', function() {
-    it('does not go below 0 when un-liking an event', function(done) {
-      request(app)
+  afterEach(function() {
+    dbStub.restore();
+  });
+  it('un-likes an event', function (done) {
+    dbStub.returns(Promise.resolve({ events: []}));
+    request(app)
       .delete('/event/like')
       .send({ id: 2 })
       .set('Accept', 'application/json')
@@ -103,17 +107,23 @@ describe('GET /', function() {
         if (err) {
           return done(err);
         }
-        chai.expect(JSON.parse(res.text).events.find(x => x.id === 2).likes).to.equal(0);
         return done();
       });
-
-      });
   });
+});
 
-  describe('DELETE /event/like', function() {
-    it('un-likes an event', function(done) {
-      request(app)
-      .post('/event/like')
+describe('PUT /event/like', function () {
+  let dbStub;
+  beforeEach(function() {
+    dbStub = sinon.stub(db, "addLike");
+  });
+  afterEach(function() {
+    dbStub.restore();
+  });
+  it('likes an event', function (done) {
+    dbStub.returns(Promise.resolve({ events: []}));
+    request(app)
+      .put('/event/like')
       .send({ id: 2 })
       .set('Accept', 'application/json')
       .expect(200)
@@ -121,21 +131,9 @@ describe('GET /', function() {
         if (err) {
           return done(err);
         }
-        request(app)
-        .delete('/event/like')
-        .send({ id: 2 })
-        .set('Accept', 'application/json')
-        .expect(200)
-        .end((err, res) => {
-          if (err) {
-            return done(err);
-          }
-          chai.expect(JSON.parse(res.text).events.find(x => x.id === 2).likes).to.equal(0);
-          return done();
-        });
-      });
-
-
-
+        return done();
       });
   });
+});
+
+
