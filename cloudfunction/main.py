60d045e44be6c7fd9778875b83f5bead8ac07d25
Kevin Rattan <kevin@time2yak.com>
2022-09-20 11:23:42 -0400

    revised function and added approval link

--- cloudfunction/main.py
+++ cloudfunction/main.py
@@ -1,3 +1,9 @@
+# This has the following dependencies in addition to enabling apis:
+# 1. two image buckets - one private from which it is triggered, 
+#                        one public read for approved thumbnails
+# 2. a pubsub topic for the message if approved
+
+
 from google.cloud import storage, vision
 from google.cloud import pubsub_v1
 from wand.image import Image
@@ -10,53 +16,37 @@ project_id =  os.environ.get("PROJECT_ID", "kr-test-work")
 topic_id =  os.environ.get("TOPIC_ID", "image_updated")
 
 def make_thumbnail(data, context):
+    # check if image is safe before doing work:
+    current_name = data['name']
+    bucket_name = data['bucket']
+    blob_uri = f'gs://{bucket_name}/{current_name}'
+    blob_source = {'source': {'image_uri': blob_uri}}
 
-    # only do any work if the image is safe:
-    safe = __check_image(data)
-
-    # name of the image in temp bucket 
-    currentName = data['name']
-    # renamed for live bucket
-    newName = f"thumbnail-{data['name']}"
-    size = 280
-
-    # Get the bucket which the image has been uploaded to
-    bucket = client.get_bucket(data['bucket'])
-    final_bucket = client.get_bucket(destination_bucket)
-    # get the image
-    thumbnail = Image(blob=bucket.get_blob(data['name']).download_as_string())
-
-    if safe:
+    print(f'Analyzing {current_name} in vision API.')
+    result = vision_client.safe_search_detection(blob_source)
+    detected = result.safe_search_annotation
+    if detected.adult == 5 or detected.violence == 5:
+        print(f'The image {current_name} was detected as inappropriate.')
+    else:
+        print(f'The image {current_name} was detected as appropriate, proceeding to resize.')        
+        # renamed for live bucket
+        new_name = f"thumbnail-{data['name']}"
+        # Get the bucket which the image has been uploaded to
+        bucket = client.get_bucket(data['bucket'])
+        final_bucket = client.get_bucket(destination_bucket)
+        # get the image
+        thumbnail = Image(blob=bucket.get_blob(data['name']).download_as_string())
         # only resize if it's bigger than our maximum size
+        size = 280
         if size < thumbnail.width:
-            print(('resizing image ' + currentName))
+            print(('resizing image ' + current_name))
             newHeight = int((size * thumbnail.height) / thumbnail.width)
             thumbnail.resize(size, newHeight)
         # save the image to the final bucket
-        thumbnail_blob = final_bucket.blob(newName)
+        thumbnail_blob = final_bucket.blob(new_name)
         thumbnail_blob.upload_from_string(thumbnail.make_blob())
         # publish message to topic
         publisher = pubsub_v1.PublisherClient()
         topic_path = publisher.topic_path(project_id, topic_id)
-        future = publisher.publish(topic_path, b'new image to approve!', image=currentName)
+        future = publisher.publish(topic_path, b'new image to approve!', image=current_name)
         print(future.result())
-    else:
-        print('Not replacing image as was inappropriate')
-
-def __check_image(file_data):
-    file_name = file_data['name']
-    bucket_name = file_data['bucket']
-    blob_uri = f'gs://{bucket_name}/{file_name}'
-    blob_source = {'source': {'image_uri': blob_uri}}
-
-    print(f'Analyzing {file_name} in vision API.')
-
-    result = vision_client.safe_search_detection(blob_source)
-    detected = result.safe_search_annotation
-
-    if detected.adult == 5 or detected.violence == 5:
-        print(f'The image {file_name} was detected as inappropriate.')
-        return False
-    else:
-        print(f'The image {file_name} was detected as OK.')
-        return True
